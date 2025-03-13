faa6e6223e9b4debfdfb5169f298901e4bab37b7
sheisa technologies <120563057+sheisatechnologies@users.noreply.github.com>
2025-03-13 15:52:41 +0530

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1733,3 +1733,27 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+
+var swiper = new Swiper(".smart_building_slider", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
+
+
